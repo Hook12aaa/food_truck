b24e16612351d669a8d2e33ebbab58e4bbb62519
Hook12aaa <anton.chepaldin@outlook.com>
2022-06-03 18:07:16 +0100

    ROS intial create

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/hook/Documents/food_truck/src
+# Build directory: /home/hook/Documents/food_truck/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
